e0a716892ba0dcb5aa9c95579463fbaf1d9cecdd
Thomas Wiik <thomas.wiik@no.experis.com>
2024-08-28 08:24:01 +0200

    Implemented language apis.

--- src/main/java/com/booleanuk/api/requests/Languages.java
+++ src/main/java/com/booleanuk/api/requests/Languages.java
@@ -2,14 +2,16 @@ package com.booleanuk.api.requests;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/languages")
 public class Languages {
-    private List<Language> languages = new ArrayList<>(){{
+    private final List<Language> languages = new ArrayList<>(){{
         add(new Language("Java"));
         add(new Language("C#"));
     }};
@@ -30,38 +32,48 @@ public class Languages {
     @GetMapping("/{name}")
     @ResponseStatus(HttpStatus.OK)
     public Language get(@PathVariable String name){
-        Language language;
-        for (Language l : languages){
-            if (l.getName().equals(name)){
-                return l;
-            }
+        try {
+            return getLanguage(name);
+        } catch (ResponseStatusException e){
+            System.out.println(e.getMessage());
+            return null;
         }
-        return null;
+    }
+
+    // Helper function to adhere to DRY and increase readability
+    public Language getLanguage(String name){
+        Optional<Language> optionalLanguage = languages.stream().filter(l -> l.getName().equals(name)).findFirst();
+        if (optionalLanguage.isPresent()){
+            return optionalLanguage.get();
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Language not found");
+        }
     }
 
     @PutMapping("/{name}")
     @ResponseStatus(HttpStatus.CREATED)
-    public Language update(@PathVariable String name, @RequestBody String newName){
-        for (Language l : languages){
-            if (l.getName().equals(name)){
-                l.setName(newName);
+    public Language update(@PathVariable String name, @RequestBody Language language){
+        Optional<Language> optionalLanguage = languages.stream().filter(l -> l.getName().equals(name)).findFirst();
+            if (optionalLanguage.isPresent()) {
+                optionalLanguage.get().setName(language.getName());
+                return optionalLanguage.get();
             }
-        }
-        return null;
+            else {
+                return null;
+            }
     }
 
     @DeleteMapping("/{name}")
     @ResponseStatus(HttpStatus.OK)
     public Language delete(@PathVariable String name){
-        Language language;
-        for (Language l : languages){
-            if (l.getName().equals(name)){
-                language = l;
-                languages.remove(l);
-                return language;
-            }
-        }
-        return null;
-    }
+        try {
+            Language language = getLanguage(name);
+            languages.remove(language);
+            return language;
 
+        } catch (ResponseStatusException e){
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
 }
